a8eeecb2b8f5fb10c6d05d6adbadb2f26fab2e74
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-07-11 20:31:01 -0300

    feat: modify styles for movies and create functions

--- netflix-clone-project/src/App.js
+++ netflix-clone-project/src/App.js
@@ -3,20 +3,37 @@ import React, { useEffect, useState } from 'react';
 import fetchsDb from './fetchsDb';
 import MovieSection from './components/MovieSection';
 import './App.css';
+import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAllList = async () => {
       let list = await fetchsDb.getHomeLists();
       setMovieList(list);
+      console.log(list);
+
+      let originals = list.filter((i) => i.slug === 'originals');
+      let randomChosem = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosem];
+      let chosenInfo = await fetchsDb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
     };
     loadAllList();
   }, []);
 
   return (
     <div className="page">
+      <Header black={blackHeader} />
+
+      {featureData && <FeatureMovie item={featureData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieSection key={key} title={item.title} items={item.items} />
--- netflix-clone-project/src/fetchsDb.js
+++ netflix-clone-project/src/fetchsDb.js
@@ -1,3 +1,4 @@
+/* eslint-disable default-case */
 /* eslint-disable import/no-anonymous-default-export */
 const API_KEY = '446422254363b2af0aa6e70f81985f45';
 const API_BASE = 'https://api.themoviedb.org/3';
@@ -36,37 +37,58 @@ export default {
         slug: 'actions',
         title: 'Ação',
         items: await fetchMovies(
-          `/discover/movie?with_genres=28&language=pt-BR$api_key=${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: 'comedy',
         title: 'Comédia',
         items: await fetchMovies(
-          `/discover/movie?with_genres=35&language=pt-BR$api_key=${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: 'horror',
         title: 'Terror',
         items: await fetchMovies(
-          `/discover/movie?with_genres=27&language=pt-BR$api_key=${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: 'romance',
         title: 'Romance',
         items: await fetchMovies(
-          `/discover/movie?with_genres=10749&language=pt-BR$api_key=${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: 'documentary',
         title: 'Documentários',
         items: await fetchMovies(
-          `/discover/movie?with_genres=99&language=pt-BR$api_key=${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
         ),
       },
     ];
+  },
+  getMovieInfo: async (movideId, type) => {
+    let info = {};
+    if (movideId) {
+      switch (type) {
+        case 'movie':
+          info = await fetchMovies(
+            `/movie/${movideId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+        case 'tv':
+          info = await fetchMovies(
+            `/tv/${movideId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+        default:
+          info = null;
+          break;
+      }
+    }
+    return info;
   },
 };
