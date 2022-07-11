2bea06e3df04ce7a36adca1b7688f13c9d7b808f
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-07-11 13:45:16 -0300

    feat: mapping movies list and styling started

--- netflix-clone-project/src/App.js
+++ netflix-clone-project/src/App.js
@@ -1,5 +1,27 @@
-import React from 'react';
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useEffect, useState } from 'react';
+import fetchsDb from './fetchsDb';
+import MovieSection from './components/MovieSection';
+import './App.css';
 
 export default () => {
-  return <div>Olá Mundo!</div>;
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAllList = async () => {
+      let list = await fetchsDb.getHomeLists();
+      setMovieList(list);
+    };
+    loadAllList();
+  }, []);
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieSection key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
