c04c9f372d39d59388bd9ba7456f8a5ccbcd3c27
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-07-11 21:43:28 -0300

    feat: add header and function scroll

--- netflix-clone-project/src/App.js
+++ netflix-clone-project/src/App.js
@@ -28,6 +28,20 @@ export default () => {
     loadAllList();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  });
+
   return (
     <div className="page">
       <Header black={blackHeader} />
