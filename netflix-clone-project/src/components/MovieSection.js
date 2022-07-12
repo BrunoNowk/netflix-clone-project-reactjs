129d23cc4f070dab82a80e001900dcaded455628
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-07-11 22:26:02 -0300

    feat: finished project

--- netflix-clone-project/src/components/MovieSection.js
+++ netflix-clone-project/src/components/MovieSection.js
@@ -1,14 +1,55 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieSection.css';
+// import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
+    }
+    setScrollX(x);
+  };
+
+  const handleRigthArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div
+        className="movieRow--left"
+        style={{ fontSize: 50 }}
+        onClick={handleLeftArrow}
+      >
+        🡂
+      </div>
+      <div
+        className="movieRow--rigth"
+        style={{ fontSize: 50 }}
+        onClick={handleRigthArrow}
+      >
+        🡀
+      </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: items.results.length * 150,
+          }}
+        >
           {items.results?.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
